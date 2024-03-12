5d0f655667776088804707c8eb7c0314b7c8d966
lucifer3049 <81276617+lucifer3049@users.noreply.github.com>
2024-03-12 23:13:44 +0800

    全局引入貨幣及時間轉換

--- src/main.js
+++ src/main.js
@@ -11,14 +11,23 @@ import VueAxios from 'vue-axios'
 
 import $httpMessageState from '@/methods/pushMessageState'
 
+import { date, currency } from '@/methods/filters'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+// 
+app.config.globalProperties.$filters={
+    date,
+    currency,
+};
+
 // 正常來說不建議太多方法掛 Global，這裡可以使用 provide 來處理
-// app.config.globalProperties.$httpMessageState = $httpMessageState
-app.provide('$httpMessageState', $httpMessageState)
+// app.config.globalProperties.$httpMessageState = $httpMessageState;
+app.provide('$httpMessageState', $httpMessageState);
+
 app.use(createPinia())
 app.use(router)
 app.use(VueAxios, axios);  //API串接套件
